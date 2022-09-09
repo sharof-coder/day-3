fe050f2a280ecb20104292c98133f27a7eb3cfcd
sharof-coder <113181174+sharof-coder@users.noreply.github.com>
2022-09-09 16:45:56 +0500

    Create app.js

---
+++ app.js
@@ -0,0 +1,76 @@
+// let a = 153245;
+// console.log(Math.trunc(a/100000)+Math.trunc(a/10000)%10+Math.trunc(a/1000)%10+Math.trunc(a/100)%10+Math.trunc(a/10)%10+a%10);
+
+                            //  HOMEWORK
+
+// let a = 8;
+// let b = 5;
+// console.log(Math.sqrt(Math.sqrt((a**3-b**3)+(a**3+b**3))));
+
+// let a = 4;
+// let b = 9;
+// console.log((1/6*Math.sqrt(a)+1/3*Math.sqrt(b))**2);
+
+// let a = 231;
+// console.log(Math.trunc(a/100)+Math.trunc(a/10)%10*10+a%10*100)
+
+let b = 538;
+// console.log(Math.trunc(b/100)*10+Math.trunc(b/10)%10*100+b%10);
+
+// let a = 4.5;
+// let b = 9.6;
+// console.log(Math.round((9*a**2*b-27*a**2*b**2+15*b**2)*100)/100);
+
+// let r = 3;
+// let n = 2;
+// let a = 4;
+// let b = 5;
+// console.log((1+r/100)**n/Math.sqrt(a**2+b**2));
+
+// let x1 = -1.4;
+// let y1 = 7.9;
+// let x2 = 6.1;
+// let y2 = 9.9;
+// console.log(Math.sqrt((x2-x1)**2 + (y2 - y1)**2));
+
+// let x1 = -9.7;
+// let y1 = -9.9;
+// let x2 = 0.0;
+// let y2 = 2.9;
+// let x3 = -0.1;
+// let y3 = 5.2;
+// let p = (x1+x2+x3)/2;
+// let math = Math.trunc(p);
+// console.log(math);
+// console.log(Math.sqrt(p*(p-x1)));
+
+// let a = 23;
+// let b = a % 10 + Math.trunc(a / 10) % 10;
+// let c = a % 10 * Math.trunc(a / 10) % 10;
+// console.log(c);
+
+// let n = 12;
+// console.log(Math.pow(2*(n+3)**2 , 1,2));
+
+// let n = 5;
+// console.log(Math.pow((3 + n) / 2,2));
+
+// let n = 4;
+// let x = 2;
+// console.log(Math.pow( n , x) + Math.pow ( 6 , x));
+
+// let m = 3.456;
+// console.log(Math.round(m * 100) / 100);
+
+// let x = 2;
+// let y = 3;
+// console.log(Math.pow(x,4) + 5*Math.pow(x , 2) + Math.pow(x,3) *y);
+
+// let x = 2;
+// let y = 3;
+// console.log(6 * Math.pow(x , 3)* Math.pow(y , 5) + 4 * Math.pow(x , 4) * Math.pow(y , 3) - 24 * x * y);
+
+// let a = 241;
+// console.log(Math.trunc(a/100) + Math.trunc(a/10)%10 + a%10 , Math.trunc(a/100) * Math.trunc(a/10)%10 * a%10);
+
+// console.log(Math.abs((3*1+2)/3 - (4*3+1)/4));
